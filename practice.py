8e5958d593d2e89efc654059ac681e8c1e7b7c76
CodeWithMihran <sohail.mihran@gmail.com>
2025-07-22 22:29:19 +0530

    Learning Python Language

---
+++ practice.py
@@ -0,0 +1,30 @@
+# print("Hello World")
+# name = input("What is your name : ")
+# print("Hello" + name)
+
+# old_age = input("Enter your old age : ")
+# new_age = int(old_age) + 2
+# print(new_age)
+
+# first = input("Enter the first number : ")
+# second = input("Enter the second number : ")
+# sum1 = first + second
+# sum2 = int(first) + int(second)
+# print("The Concatinated Sum is : "+ str(sum1))
+# print("The Real Sum is : "+ str(sum2))
+
+# name = "Tony Stark"
+# print(name.upper())
+# print(name.lower())
+# print(name.find("S"))
+# print(name.find("s"))
+# print(name.replace("Tony Stark","Iron Man"))
+# print(name.replace("T","M"))
+# print('T' in name)
+# print('M' in name)
+
+# age = input("Enter your age : ")
+
+# if int(age) >= 18:
+#     print("You are eligible for voting.")
+# print("ThankYou")
